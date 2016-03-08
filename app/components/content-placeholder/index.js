aa9cbaea5677093151c4059bad149536249d0b6d
Teffen Ellis (Eric) <teffen@nirri.us>
2016-03-07 16:08:05 -0800

    Prevent placeholder from flashing between fast page loads.

--- app/components/content-placeholder/index.js
+++ app/components/content-placeholder/index.js
@@ -2,11 +2,25 @@ import "./content-placeholder.styl"
 import logo from "../../images/foundation/text-logo.svg"
 // Placeholder component. Used to fill in loading wait time.
 
-import React from "react"
+import React, {Component} from "react"
 
-export default React.createClass({
+const VISIBILITY_DELAY = 4000
+
+export default class ContentPlaceholder extends Component {
+  state = {
+    visible: false
+  };
+
+  componentDidMount() {
+    this.visibilityTimeout = setTimeout(() => this.setState({visible: true}), VISIBILITY_DELAY)
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.visibilityTimeout)
+  }
+
   render() {
-    return <section data-component="content-placeholder">
+    return <section data-component="content-placeholder" data-visible={this.state.visible ? "" : null}>
       <img
         className="alternative-fiction-text-logo"
         src={logo} />
@@ -19,4 +33,4 @@ export default React.createClass({
       </div>
     </section>
   }
-})
+}
