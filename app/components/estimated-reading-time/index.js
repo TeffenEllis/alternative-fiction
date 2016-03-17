208c621beedf797c95d615e57c947ebb590f3db9
Teffen Ellis (Eric) <teffen@nirri.us>
2016-03-16 23:52:21 -0700

    Remove jquery dependence for scroll calculation.

--- app/components/estimated-reading-time/index.js
+++ app/components/estimated-reading-time/index.js
@@ -3,66 +3,77 @@
 // * text: String. Estimation is based off this.
 // * textComponent: React Component. Subtract's scrolled percentage of text if given.
 
-import React from "react"
-import $ from "jquery"
-
+import React, {Component} from "react"
 import moment from "moment"
-import {size, debounce} from "lodash"
 
 const WORDS_PER_MINUTE = 250
-const SCROLL_THROTTLE = 500
 
-export default React.createClass({
+function getOffsetTop(element) {
+  let offsetTop = 0
+
+  do {
+    if (!isNaN(element.offsetTop)) offsetTop += element.offsetTop
+  }
+  while (element = element.offsetParent) // eslint-disable-line no-cond-assign
+
+  return offsetTop
+}
+
+function getTextEstimates(text, percentageRead) {
+  const spaces = text.match(/\s+/g)
+  const wordCount = spaces ? spaces.length : 0
+  const minutes = wordCount / WORDS_PER_MINUTE * (1 - percentageRead)
+
+  return {minutes, wordCount}
+}
+
+export default class EstimatedReadingTime extends Component {
+  static defaultProps = {
+    trackScrollPosition: false
+  };
+
+  state = {
+    scrollPercentage: 0
+  };
+
   componentDidMount() {
     if (!this.props.trackScrollPosition) return
 
-    // Calculating scroll position without jQuery is incredibly frustrating.
-    // Each browser handles window scrolling dimensions differently.
-    // The throttled function must be in a callback because React isn't detecting
-    // that an update should be made.
+    this.scrollHandler = this._updateScrollPercentage.bind(this)
 
-    const throttledUpdate = debounce(this.forceUpdate.bind(this), SCROLL_THROTTLE)
-
-    $(window).scroll(() => throttledUpdate())
-  },
+    document
+      .querySelector("#content-container")
+      .addEventListener("scroll", this.scrollHandler)
+  }
 
   componentWillUnmount() {
-    $(window).unbind("scroll")
-  },
+    window.removeEventListener(this.scrollHandler)
+  }
 
-  estimateTimeByText(text = "") {
-    const wordCount = size(text.match(/\s+/g))
-    const minutes = wordCount / WORDS_PER_MINUTE * (1 - this.percentageRead())
+  _updateScrollPercentage() {
+    const element = this.props.textComponent
 
-    return {wordCount, minutes}
-  },
+    this.parent = this.parent || document.querySelector("#content-container")
 
-  getDefaultProps() {
-    return {
-      trackScrollPosition: false
-    }
-  },
+    // Consider the element's offset from the body and the portion visible from the viewport.
+    const offsetTop = getOffsetTop(element) - document.documentElement.clientHeight
+    // Consider if the element is beyond the viewport.
+    const currentY = Math.max(this.parent.scrollTop - offsetTop, 0)
+    const scrollPercentage = currentY / (element.scrollHeight || element.clientHeight)
 
-  percentageRead() {
-    if (!this.props.textComponent) return 0
-
-    const $this = $(window)
-    const currentY = $this.scrollTop()
-    const windowHeight = $this.height()
-    const scrollHeight = $(this.props.textComponent).height()
-    const scrollPercent = currentY / (scrollHeight - windowHeight)
-
-    // Percentage will be higher than 1 if the window is scrolled beyond the element.
-    return Math.min(scrollPercent, 1)
-  },
+    // Consider if the body is scrolled beyond the element.
+    this.setState({scrollPercentage: Math.min(scrollPercentage, 1)})
+  }
 
   render() {
-    if (!this.props.textComponent) {
+    const {textComponent} = this.props
+
+    if (!textComponent) {
       return <span className="parent-component-not-ready" data-component="unresolved" />
     }
 
     let label = ""
-    const {minutes, wordCount} = this.estimateTimeByText(this.props.textComponent.innerHTML)
+    const {minutes, wordCount} = getTextEstimates(textComponent.textContent, this.state.scrollPercentage)
 
     if (wordCount < WORDS_PER_MINUTE) {
       label = "A few seconds" // Very short text.
@@ -78,5 +89,4 @@ export default React.createClass({
       {label}
     </span>
   }
-})
-
+}
